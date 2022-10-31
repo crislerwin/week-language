991fe0115535b4b8b404f8294a7ff0746a598dad
crislerwin <crisler.wintler@splitc.com.br>
2022-10-30 21:06:20 -0400

    ✅ update guessing game to use rand

--- rust/src/guessing_game.rs
+++ rust/src/guessing_game.rs
@@ -1,17 +1,20 @@
 use std::io;
+use rand::Rng;
+
 
 pub(crate) fn run() {
     println!("Guess the number!");
     
-    print!("Please input your guess: ");
-
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    print!("The secret number is: {secret_number} ");
+    print!("Please input your guess.");
+    
+   let mut guess = String::new();
+   io::stdin() .read_line(&mut guess)
+   .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+println!("You guessed: {guess}");
     
 }
 
